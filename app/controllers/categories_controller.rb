0d5846729f7345471951d391b1d7e474ff988e52
Marc Garreau <marcdgarreau@gmail.com>
2014-08-13 14:41:30 -0600

    Adds ability to add a category with tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,6 +16,15 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def new
+		@category = Category.new
+	end
+
+	def create 
+		Category.create(category_params)
+		redirect_to categories_path
+	end
+
 	private
 
 	def category_params
--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -21,8 +21,19 @@ describe 'when viewing the categories' do
 		it 'can edit a category' do
 			click_link 'edit'
 			fill_in "Name", with: "Changed Donut"
-			click_button 'Update'
+			click_button 'Update Category'
 			expect(page).to have_content("Changed Donut")
+		end
+
+		it 'has a link to add a category' do
+			expect(page).to have_link('Add Category')
+		end
+
+		it 'can add a category' do
+			click_link 'Add Category'
+			fill_in "Name", with: 'New Category'
+			click_button 'Create Category'
+			expect(page).to have_content('New Category')
 		end
 
 	end
