a524bb97da3913cedd259059886c53580f797cf3
Andrew Watkins <andrew.watkinz@gmail.com>
2014-08-13 16:02:57 -0600

    email uniqueness testing pass

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -18,14 +18,30 @@ describe '' do
       expect(page).to have_content "Email can't be blank"
     end
 
-    it 'cannot register without a password' do
-      register(password: nil)
-      expect(page).to have_content "Password can't be blank"
+    it 'cannot register with an email already registersed' do
+      register
+      register
+      expect(page).to have_content "Email has already been taken"
     end
 
     it 'cannot register with an invalid email address' do
       register(email: '^$%^%#$@#$^%$^.com')
       expect(page).to have_content "Email is invalid"
+    end
+
+    it 'cannot register without a password' do
+      register(password: nil)
+      expect(page).to have_content "Password can't be blank"
+    end
+
+    it 'cannot register without a password confirmation' do
+      register(password_confirmation: nil)
+      expect(page).to have_content "Password confirmation can't be blank"
+    end
+
+    it 'cannnot register when passwords do not match' do
+      register(password: '123', password_confirmation: '1234')
+      expect(page).to have_content "Password confirmation doesn't match Password"
     end
   end
 end
