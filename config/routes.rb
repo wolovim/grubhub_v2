0ab81f7c6ab7225f32aa9e4f07da65877252be8d
Andrew Watkins <andrew.watkinz@gmail.com>
2014-08-14 16:01:42 -0600

    starts authentication -- broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   resources :items
   resources :categories
+
   resources :orders do
     get '/update_status' => 'orders#update_status', as: :update_status
     get '/cancel' => 'orders#cancel', as: :cancel
     get '/remove_item/:item_id' => 'orders#remove_item', as: :remove_item
   end
+
   resources :order_items, only: [:destroy, :update]
   resources :users,  only: [:new, :create]
   resources :addresses, only: [:new, :create, :index]
+
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   root to: 'categories#index'
 end
