f80879a19a2cce0f50b4b7776d614ae240eb0903
Marc Garreau <marcdgarreau@gmail.com>
2014-08-18 15:07:09 -0600

    Adds initial namespacing for admin and admin orders

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,46 @@
+class Admin::OrdersController < ApplicationController
+	def index
+		@orders = Order.all
+	end
+
+	def show
+		@order = Order.find(params[:id])
+	end
+
+	def edit
+		@order = Order.find(params[:id])
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		if @order.update(order_params)
+			redirect_to orders_path
+		else
+			render :edit
+		end
+	end
+
+	def cancel
+		@order = Order.find(params[:order_id])
+		@order.cancel
+		redirect_to orders_path
+	end
+
+	def update_status
+		@order = Order.find(params[:order_id])
+		@order.update_status
+		redirect_to orders_path
+	end
+
+	def remove_item
+		@order = Order.find(params[:order_id])
+		@order.remove_item(params[:item_id])
+		redirect_to edit_order_path(@order)
+	end
+
+	private
+
+	def order_params
+		params.require(:order).permit(:user_id, :order_type, :address_id, :status, :total)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,15 @@ Rails.application.routes.draw do
   end
   resources :order_items, only: [:destroy, :update]
   root to: 'categories#index'
+
+  namespace :admin do
+  	resources :orders do
+	    get '/update_status' => 'admin/orders#update_status', as: :update_status
+	    get '/cancel' => 'admin/orders#cancel', as: :cancel
+	    get '/remove_item/:item_id' => 'admin/orders#remove_item', as: :remove_item
+	  end
+  end
+
+  get '/admin/' => 'admin/orders#index' # How to set namespace specific controller?
+
 end
