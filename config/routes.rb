45c3aa2756b412cb453b2d5053a466cbb5b06468
Jonmichael Chambers <jon@tyrbo.net>
2014-08-18 14:04:07 -0600

    Finishes log out

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   get '/login',  to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   root to: 'categories#index'
 end
--- spec/features/log_in_out_spec.rb
+++ spec/features/log_in_out_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
-describe 'loggin in' do
+describe 'Logging in' do
 
-  context 'as a guest' do
+  before(:each) { User.create email: 'yourmom123@aol.com',
+                  password: '123',
+                  password_confirmation: '123',
+                  first_name: 'Jon',
+                  last_name:  'Michael' }
 
-    before(:each) { User.create email: 'yourmom123@aol.com',
-                                password: '123',
-                                password_confirmation: '123',
-                                first_name: 'Jon',
-                                last_name:  'Michael' }
-    it 'successfully' do
-      visit login_path
-      fill_in 'Email', with: 'yourmom123@aol.com'
-      fill_in 'Password', with: '123'
-      click_button 'Login'
+  context 'as a Guest' do
+    it 'logs in successfully' do
+      login
       expect(page).to_not have_content 'Login'
       expect(page).to     have_content 'Logout'
     end
+  end
 
+  context 'as an Authenticated User' do
+    it 'logs out' do
+      login
+      click_on 'Logout'
+      expect(page).to     have_content 'Login'
+      expect(page).to_not have_content 'Logout'
+    end
   end
 end
