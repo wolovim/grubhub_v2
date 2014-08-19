8fd1a5cfd25bd9485c8a3141530f48b730b3a931
Jonmichael Chambers <jon@tyrbo.net>
2014-08-19 14:47:20 -0600

    Adds simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
