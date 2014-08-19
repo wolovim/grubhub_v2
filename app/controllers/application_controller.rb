738551222b506d7e01ebf4e5887893be826f0a39
Jonmichael Chambers <jon@tyrbo.net>
2014-08-19 14:23:18 -0600

    Adds is_admin? helper method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :is_admin?
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def is_admin?
+    current_user && current_user.role == 'admin'
   end
 
   def check_admin
-    user = current_user || User.new(role: 'guest')
-
-    unless user.role == 'admin'
+    unless is_admin?
       flash[:error] = 'You must be logged in to access that.'
       session[:last_page] = request.path
       redirect_to login_path
