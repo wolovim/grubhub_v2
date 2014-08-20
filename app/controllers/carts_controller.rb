b1692cffcbe1bfca29c9ad21712cd504efb556fb
Jonmichael Chambers <jon@tyrbo.net>
2014-08-20 09:37:05 -0600

    Users cannot add items that do not exist or are disabled

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
   def update
-    current_cart << params[:item_id]
+    if (item = Item.find_by(id: params[:item_id])) && item.enabled?
+      current_cart << params[:item_id]
+    else
+      flash[:error] = 'That item is no longer available.'
+    end
     redirect_to categories_path
   end
 end
