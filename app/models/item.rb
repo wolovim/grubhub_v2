d3b73db2a38552c504c21eb8cd276d43391c81e0
Chad Brading <chadbrading@gmx.com>
2014-08-25 13:26:52 -0600

    can not create multiple items with the same name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
 	validates :title, :description, :price, presence: true
+	validates :title, uniqueness: true
 	has_many :item_categories
 	has_many :categories, through: :item_categories
 	has_many :orders, through: :order_items
