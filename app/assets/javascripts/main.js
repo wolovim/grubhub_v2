1466284bf8360ca8590fd24ff914ad1575afd10b
Marc Garreau <marcdgarreau@gmail.com>
2014-08-26 17:52:01 -0600

    Fixes jquery bug and styles login and register pages

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	// "Sticky" sidebar on main menu
+	
+	  $("#sticker").sticky({ topSpacing: 80 });
+	
+
+  // Checkout form: hide/show delivery address & credit card form
+  $("#address-selector").hide();
+  $("#payment-selector").hide();
+
+	$("#pickup-radio").on("click", function() {
+	   $("#address-selector").slideUp();
+	});
+
+	$("#delivery-radio").on("click", function() {
+	   $("#address-selector").slideDown(); 
+	});
+
+	$("#cash-radio").on("click", function() {
+	   $("#payment-selector").slideUp();
+	});
+
+	$("#credit-radio").on("click", function() {
+	   $("#payment-selector").slideDown(); 
+	});
+
+	$("#saved-address-selector").hide();
+	$("#new-address-form").hide();
+
+	$("#saved-address-radio").on("click", function () {
+		$("#saved-address-selector").slideDown();
+		$("#new-address-form").slideUp();
+	});
+
+	$("#new-address-radio").on("click", function () {
+		$("#new-address-form").slideDown();
+		$("#saved-address-selector").slideUp();
+	});
+
+});
