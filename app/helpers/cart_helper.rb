44565573a86479165b5284ecbf7887652d015e6b
Jonmichael Chambers <jon@tyrbo.net>
2014-08-26 09:42:07 -0600

    Added a calculate_total helper for the checkout page.

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,4 +1,11 @@
 module CartHelper
+  def calculate_total
+    current_cart.inject(0) do |sum, (id, quantity)|
+      item = Item.find(id)
+      sum += item_subtotal(item.id, item.price)
+    end
+  end
+
   def cart_count
     current_cart.inject(0) { |sum, (_, x)| sum + x }
   end
@@ -8,6 +15,6 @@ module CartHelper
   end
 
   def item_subtotal(item_id, item_price)
-    price_in_dollars(item_quantity(item_id) * item_price)
+    item_quantity(item_id) * item_price
   end
 end
