0a01fef49c07f90c2d035a477501cb822ba24582
Marc Garreau <marcdgarreau@gmail.com>
2014-08-27 12:04:22 -0600

    Adds default image when an item is created without one

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ActiveRecord::Base
 	has_many :categories, through: :item_categories
 	has_many :orders, through: :order_items
 
-	has_attached_file :image, styles: {:small => "150x150>", :thumb => "100x100>"}
+	has_attached_file :image, styles: {:small => "150x150>", :thumb => "100x100>"}, default_url: "/assets/images/missing-donut.jpg"
 	validates_attachment :image, content_type: {content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]}
 
 	def display_categories
