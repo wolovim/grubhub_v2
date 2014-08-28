0bab89d51b94ce1b19d72863e81d05941d05528f
Jonmichael Chambers <jon@tyrbo.net>
2014-08-28 08:54:02 -0600

    Removed orders from seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,70 +60,70 @@ rachel_address3 = Address.create(street: '101 Stout St', city: 'Denver', state: 
 jorge_address = Address.create(street: '23 Colfax Ave', city: 'Denver', state: 'CO', zip: '80205', user: jorge)
 
 #ORDERS
-Order.create(user: rachel, order_type: 'delivery', payment_type: 'cash', address: rachel_address1, status: "ordered")
-Order.create(user: rachel, order_type: 'delivery', payment_type: 'credit', address: rachel_address2, status: "ordered")
-Order.create(user: jeff, order_type: 'delivery', payment_type: 'credit', address: jeff_address1, status: "ordered")
-Order.create(user: jeff, order_type: 'delivery', payment_type: 'credit', address: jeff_address1, status: "ordered")
-Order.create(user: jeff, order_type: 'delivery', payment_type: 'cash', address: jeff_address1, status: "cancelled")
-Order.create(user: jorge, order_type: 'delivery', payment_type: 'cash', address: jorge_address, status: "cancelled")
-Order.create(user: jorge, order_type: 'delivery', payment_type: 'cash', address: jorge_address, status: "paid")
-Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "completed")
-Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "ordered")
-Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "paid")
-
-
-##ORDER ITEMS
-##1
-OrderItem.create(item_id: 1,
-  order_id: 1, quantity: 3, unit_price: 2400)
-OrderItem.create(item_id: 2,
-  order_id: 1, quantity: 4, unit_price: 2000)
-OrderItem.create(item_id: 3,
-  order_id: 1, quantity: 5, unit_price: 5000)
-##2
-OrderItem.create(item_id: 4,
-  order_id: 2, quantity: 1, unit_price: 3000)
-OrderItem.create(item_id: 5,
-  order_id: 2, quantity: 15, unit_price: 2050)
-OrderItem.create(item_id: 6,
-  order_id: 2, quantity: 4, unit_price: 1000)
-##3
-OrderItem.create(item_id: 6,
-  order_id: 3, quantity: 7, unit_price: 600)
-OrderItem.create(item_id: 10,
-  order_id: 3, quantity: 1, unit_price: 5000)
-##4
-OrderItem.create(item_id: 1,
-  order_id: 4, quantity: 1, unit_price: 3770)
-OrderItem.create(item_id: 9,
-  order_id: 4, quantity: 1, unit_price: 1110)
-##5
-OrderItem.create(item_id: 13,
-  order_id: 5, quantity: 1, unit_price: 8000)
-##6
-OrderItem.create(item_id: 12,
-  order_id: 6, quantity: 6, unit_price: 1230)
-OrderItem.create(item_id: 11,
-  order_id: 6, quantity: 2, unit_price: 1110)
-OrderItem.create(item_id: 10,
-  order_id: 6, quantity: 2, unit_price: 8900)
-##7
-OrderItem.create(item_id: 9,
-  order_id: 7, quantity: 2, unit_price: 4000)
-##8
-OrderItem.create(item_id: 8,
-  order_id: 8, quantity: 20, unit_price: 8000)
-##9
-OrderItem.create(item_id: 11,
-  order_id: 9, quantity: 1, unit_price: 4440)
-OrderItem.create(item_id: 12,
-  order_id: 10, quantity: 2, unit_price: 1230)
-OrderItem.create(item_id: 3,
-  order_id: 11, quantity: 3, unit_price: 5000)
-OrderItem.create(item_id: 10,
-  order_id: 13, quantity: 4, unit_price: 1100)
-##10
-OrderItem.create(item_id: 2,
-  order_id: 12, quantity: 5, unit_price: 8000)
-OrderItem.create(item_id: 3,
-  order_id: 14, quantity: 6, unit_price: 1000)
+#rachel_order1 = Order.create(user: rachel, order_type: 'delivery', payment_type: 'cash', address: rachel_address1, status: "ordered")
+#Order.create(user: rachel, order_type: 'delivery', payment_type: 'credit', address: rachel_address2, status: "ordered")
+#Order.create(user: jeff, order_type: 'delivery', payment_type: 'credit', address: jeff_address1, status: "ordered")
+#Order.create(user: jeff, order_type: 'delivery', payment_type: 'credit', address: jeff_address1, status: "ordered")
+#Order.create(user: jeff, order_type: 'delivery', payment_type: 'cash', address: jeff_address1, status: "cancelled")
+#Order.create(user: jorge, order_type: 'delivery', payment_type: 'cash', address: jorge_address, status: "cancelled")
+#Order.create(user: jorge, order_type: 'delivery', payment_type: 'cash', address: jorge_address, status: "paid")
+#Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "completed")
+#Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "ordered")
+#Order.create(user: jorge, order_type: 'pickup', payment_type: 'cash', address: jorge_address, status: "paid")
+#
+#
+###ORDER ITEMS
+###1
+#rachel_order1.order_items.create(item_id: 1,
+#  order_id: 1, quantity: 3, unit_price: 2400)
+#OrderItem.create(item_id: 2,
+#  order_id: 1, quantity: 4, unit_price: 2000)
+#OrderItem.create(item_id: 3,
+#  order_id: 1, quantity: 5, unit_price: 5000)
+###2
+#OrderItem.create(item_id: 4,
+#  order_id: 2, quantity: 1, unit_price: 3000)
+#OrderItem.create(item_id: 5,
+#  order_id: 2, quantity: 15, unit_price: 2050)
+#OrderItem.create(item_id: 6,
+#  order_id: 2, quantity: 4, unit_price: 1000)
+###3
+#OrderItem.create(item_id: 6,
+#  order_id: 3, quantity: 7, unit_price: 600)
+#OrderItem.create(item_id: 10,
+#  order_id: 3, quantity: 1, unit_price: 5000)
+###4
+#OrderItem.create(item_id: 1,
+#  order_id: 4, quantity: 1, unit_price: 3770)
+#OrderItem.create(item_id: 9,
+#  order_id: 4, quantity: 1, unit_price: 1110)
+###5
+#OrderItem.create(item_id: 13,
+#  order_id: 5, quantity: 1, unit_price: 8000)
+###6
+#OrderItem.create(item_id: 12,
+#  order_id: 6, quantity: 6, unit_price: 1230)
+#OrderItem.create(item_id: 11,
+#  order_id: 6, quantity: 2, unit_price: 1110)
+#OrderItem.create(item_id: 10,
+#  order_id: 6, quantity: 2, unit_price: 8900)
+###7
+#OrderItem.create(item_id: 9,
+#  order_id: 7, quantity: 2, unit_price: 4000)
+###8
+#OrderItem.create(item_id: 8,
+#  order_id: 8, quantity: 20, unit_price: 8000)
+###9
+#OrderItem.create(item_id: 11,
+#  order_id: 9, quantity: 1, unit_price: 4440)
+#OrderItem.create(item_id: 12,
+#  order_id: 10, quantity: 2, unit_price: 1230)
+#OrderItem.create(item_id: 3,
+#  order_id: 11, quantity: 3, unit_price: 5000)
+#OrderItem.create(item_id: 10,
+#  order_id: 13, quantity: 4, unit_price: 1100)
+###10
+#OrderItem.create(item_id: 2,
+#  order_id: 12, quantity: 5, unit_price: 8000)
+#OrderItem.create(item_id: 3,
+#  order_id: 14, quantity: 6, unit_price: 1000)
