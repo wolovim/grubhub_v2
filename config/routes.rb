8e12c4c6a66ab61e4d7894f0cfc03d2f7071ac15
Jonmichael Chambers <jon@tyrbo.net>
2014-08-27 19:29:26 -0600

    Cleaning up the routes a little bit

--- config/routes.rb
+++ config/routes.rb
@@ -17,27 +17,27 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    get '/' => 'orders#index'
+
     resources :items do
       get '/delete_category/:category_id' => 'items#delete_category', as: :delete_category
       get '/add_category/:category_id' => 'items#add_category', as: :add_category
       get '/retire' => 'items#retire', as: :retire
     end
-    resources :categories
+
+    resources :categories, except: [:show]
     resources :order_items, only: [:destroy, :update]
-    resources :users,  only: [:new, :create]
-    resources :addresses, only: [:new, :create, :index]
-  	resources :orders do
+  	resources :orders, except: [:new, :destroy] do
 	    get '/update_status' => 'orders#update_status', as: :update_status
 	    get '/cancel' => 'orders#cancel', as: :cancel
 	    get '/remove_item/:item_id' => 'orders#remove_item', as: :remove_item
 	  end
+
     get '/ordered' => 'orders#ordered', as: :ordered
     get '/paid' => 'orders#paid', as: :paid
     get '/completed' => 'orders#completed', as: :completed
     get '/cancelled' => 'orders#cancelled', as: :cancelled
   end
 
-  get '/admin', to: redirect('admin/orders#index')
   get '/code', to: redirect('https://github.com/MarcGarreau/grubhub_v2')
-
 end
